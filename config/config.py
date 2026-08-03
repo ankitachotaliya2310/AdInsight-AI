49a9de15ac896fe1367193f39b312e237c060690
Ankita <ankitachotaliya32@gmail.com>
2026-08-03 16:00:18 +0530

    Add project configuration and improve folder structure

---
+++ config/config.py
@@ -0,0 +1,19 @@
+"""
+=========================================================
+Project Configuration
+Project : AdInsight AI
+Author  : Ankita
+=========================================================
+"""
+
+# Google Cloud Project
+PROJECT_ID = "adinsight-ai-504406"
+
+# BigQuery Dataset
+DATASET = "google_analytics_sample"
+
+# Data Paths
+RAW_DATA_PATH = "../data/raw/"
+PROCESSED_DATA_PATH = "../data/processed/"
+
+MASTER_DATASET = "../data/processed/master_dataset.parquet"
